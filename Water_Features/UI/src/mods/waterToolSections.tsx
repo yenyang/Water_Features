6c514a8592f72e7ef20ae76bd283d9547fea976b
yenyang <tjkelley19@gmail.com>
2024-03-02 11:51:47 -0800

    worked out formatting of react components.

--- Water_Features/UI/src/mods/waterToolSections.tsx
+++ Water_Features/UI/src/mods/waterToolSections.tsx
@@ -9,19 +9,17 @@ export const WaterToolComponent = (moduleRegistry: ModuleRegistry) => (Component
     // The module registrys are found by logging console.log('mr', moduleRegistry); in the index file and finding appropriate one.
     const toolMouseModule = moduleRegistry.registry.get("game-ui/game/components/tool-options/mouse-tool-options/mouse-tool-options.tsx");
     const toolButtonModule = moduleRegistry.registry.get("game-ui/game/components/tool-options/tool-button/tool-button.tsx")!!;
-    const theme = moduleRegistry.registry.get("game-ui/game/components/tool-options/tool-button/tool-button.module.scss")?.classes;
     const mouseToolTheme = moduleRegistry.registry.get("game-ui/game/components/tool-options/mouse-tool-options/mouse-tool-options.module.scss")?.classes;
     // These are found in the minified JS file after searching for module.
     const Section: any = toolMouseModule?.Section;
     const StepToolButton: any = toolButtonModule?.StepToolButton;
-    const ValueToolButton: any = toolButtonModule?.ValueToolButton;
+    const ToolButton: any = toolButtonModule?.ToolButton;
 
     return (props: any) => {
         const { children, ...otherProps} = props || {};
         const { api: { api: { useValue, bindValue, trigger } } } = useModding();
         const { engine } = useModding();
 
-        console.log("Hello");
         // These establish the binding with C# side. Without C# side game ui will crash.
 
         // This binding is for whether the tool is active.
@@ -38,6 +36,7 @@ export const WaterToolComponent = (moduleRegistry: ModuleRegistry) => (Component
             trigger("Anarchy", "AnarchyToggled");
         }, [])
         
+        const showMinDepth : boolean = false;
         // This will return original component and children if there is nothing to insert.
          /*if (!toolActive) {
             return (
@@ -55,9 +54,30 @@ export const WaterToolComponent = (moduleRegistry: ModuleRegistry) => (Component
             Tooltip is translated based on localization key. OnSelect run callback fucntion here to trigger event. 
             Anarchy specific image source changes bases on Anarchy Enabled binding. 
             */
-            <Section title="Flow">
-                <ValueToolButton className = {mouseToolTheme.numberField} value = "1"></ValueToolButton>
-            </Section>)
-        return <>{result}</>;
+            <>
+                <Section title="Flow">
+                    <ToolButton className={mouseToolTheme.startButton} tooltip={"down"} onSelect={handleClick} src="coui://uil/Standard/ArrowDownThickStroke.svg"></ToolButton>
+                    <div className={mouseToolTheme.numberField}>1 m</div>
+                    <ToolButton className={mouseToolTheme.endButton} tooltip={"up"} onSelect={handleClick} src="coui://uil/Standard/ArrowUpThickStroke.svg"></ToolButton>
+                    <StepToolButton class name={mouseToolTheme.indicator} tooltip={"step"} onSelect={handleClick} values={[1.0, 0.5, 0.25, 0.125]} selectedValue={0.25}></StepToolButton>
+                </Section>
+                { showMinDepth? 
+                    <Section title="Flow">
+                        <ToolButton className={mouseToolTheme.startButton} tooltip={"down"} onSelect={handleClick} src="coui://uil/Standard/ArrowDownThickStroke.svg"></ToolButton>
+                        <div className={mouseToolTheme.numberField}>1 m</div>
+                        <ToolButton className={mouseToolTheme.endButton} tooltip={"up"} onSelect={handleClick} src="coui://uil/Standard/ArrowUpThickStroke.svg"></ToolButton>
+                        <StepToolButton class name={mouseToolTheme.indicator} tooltip={"step"} onSelect={handleClick} values={[1.0, 0.5, 0.25, 0.125]} selectedValue={0.25}></StepToolButton>
+                    </Section> 
+                    : <></>
+                }
+                <Section title="Radius">
+                    <ToolButton className={mouseToolTheme.startButton} tooltip={"down"} onSelect={handleClick} src="coui://uil/Standard/ArrowDownThickStroke.svg"></ToolButton>
+                    <div className={mouseToolTheme.numberField}>5 m</div>
+                    <ToolButton className={mouseToolTheme.endButton} tooltip={"up"} onSelect={handleClick} src="coui://uil/Standard/ArrowUpThickStroke.svg"></ToolButton>
+                    <StepToolButton class name={mouseToolTheme.indicator} tooltip={"step"} onSelect={handleClick} values={[1.0, 0.5, 0.25, 0.125]} selectedValue={0.25}></StepToolButton>
+                </Section>
+            </>
+            )
+        return result;
     };
 }
