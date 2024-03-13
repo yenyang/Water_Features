d773e566ab078816fd02b580caccf50aa2f2946a
yenyang <tjkelley19@gmail.com>
2024-03-13 08:51:05 -0700

    added updated phases for editorSimulation

--- Water_Features/WaterFeaturesMod.cs
+++ Water_Features/WaterFeaturesMod.cs
@@ -87,25 +87,38 @@ namespace Water_Features
             Log.Info("Handling create world");
             Log.Info("ModInstallFolder = " + ModInstallFolder);
             Log.Info($"{nameof(WaterFeaturesMod)}.{nameof(OnLoad)} Injecting systems.");
+            updateSystem.UpdateAt<AddPrefabsSystem>(SystemUpdatePhase.PrefabUpdate);
+
+            updateSystem.UpdateAt<WaterToolUISystem>(SystemUpdatePhase.UIUpdate);
             updateSystem.UpdateAt<CustomWaterToolSystem>(SystemUpdatePhase.ToolUpdate);
             updateSystem.UpdateAfter<WaterTooltipSystem>(SystemUpdatePhase.UITooltip);
+
             updateSystem.UpdateBefore<FindWaterSourcesSystem>(SystemUpdatePhase.GameSimulation);
             updateSystem.UpdateAt<AutofillingLakesSystem>(SystemUpdatePhase.GameSimulation);
             updateSystem.UpdateAt<DetentionBasinSystem>(SystemUpdatePhase.GameSimulation);
             updateSystem.UpdateAt<RetentionBasinSystem>(SystemUpdatePhase.GameSimulation);
             updateSystem.UpdateBefore<ChangeWaterSystemValues>(SystemUpdatePhase.GameSimulation);
             updateSystem.UpdateAt<TidesAndWavesSystem>(SystemUpdatePhase.GameSimulation);
-            updateSystem.UpdateBefore<BeforeSerializeSystem>(SystemUpdatePhase.Serialize);
-            updateSystem.UpdateAfter<TidesAndWavesSystem>(SystemUpdatePhase.Serialize);
             updateSystem.UpdateAt<SeasonalStreamsSystem>(SystemUpdatePhase.GameSimulation);
             updateSystem.UpdateAt<DisableSeasonalStreamSystem>(SystemUpdatePhase.GameSimulation);
             updateSystem.UpdateAt<DisableWavesAndTidesSystem>(SystemUpdatePhase.GameSimulation);
+
+            updateSystem.UpdateBefore<BeforeSerializeSystem>(SystemUpdatePhase.Serialize);
+            updateSystem.UpdateAfter<TidesAndWavesSystem>(SystemUpdatePhase.Serialize);
             updateSystem.UpdateAfter<SeasonalStreamsSystem>(SystemUpdatePhase.Serialize);
             updateSystem.UpdateAfter<AutofillingLakesSystem>(SystemUpdatePhase.Serialize);
             updateSystem.UpdateAfter<DetentionBasinSystem>(SystemUpdatePhase.Serialize);
             updateSystem.UpdateAfter<RetentionBasinSystem>(SystemUpdatePhase.Serialize);
-            updateSystem.UpdateAt<AddPrefabsSystem>(SystemUpdatePhase.PrefabUpdate);
-            updateSystem.UpdateAt<WaterToolUISystem>(SystemUpdatePhase.UIUpdate);
+
+            updateSystem.UpdateBefore<ChangeWaterSystemValues>(SystemUpdatePhase.EditorSimulation);
+            updateSystem.UpdateBefore<FindWaterSourcesSystem>(SystemUpdatePhase.EditorSimulation);
+            updateSystem.UpdateAt<AutofillingLakesSystem>(SystemUpdatePhase.EditorSimulation);
+            updateSystem.UpdateAt<DetentionBasinSystem>(SystemUpdatePhase.EditorSimulation);
+            updateSystem.UpdateAt<RetentionBasinSystem>(SystemUpdatePhase.EditorSimulation);
+            updateSystem.UpdateAt<TidesAndWavesSystem>(SystemUpdatePhase.EditorSimulation);
+            updateSystem.UpdateAt<SeasonalStreamsSystem>(SystemUpdatePhase.EditorSimulation);
+            updateSystem.UpdateAt<DisableSeasonalStreamSystem>(SystemUpdatePhase.EditorSimulation);
+            updateSystem.UpdateAt<DisableWavesAndTidesSystem>(SystemUpdatePhase.EditorSimulation);
             Log.Info($"{nameof(WaterFeaturesMod)}.{nameof(OnLoad)} Completed.");
         }
 
